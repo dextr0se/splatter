bdb0746687ad0e6cbfaf2ad65a4ebc0ff26d288a
coffee.h <huffmanj573@gmail.com>
2018-06-07 20:01:18 -0400

    Cleaned up some of the code
    created a new files folder
    Moved the html header to a new file in the files directory
    cleaned up the variable names
    added better comments
    added status messages

--- splatter.py
+++ splatter.py
@@ -1,85 +1,82 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('xml/references.xml')
+import xml.etree.ElementTree as et
+tree = et.parse('xml/references.xml')
 root = tree.getroot()
 
-def countNumberSections(subject):
-    sectionCounter = 0
-    for section in subject:
-        sectionCounter += 1
-    return sectionCounter
+# READ IN THE HTML HEAD
+try:
+    headFile = open('files/html-head', 'r')
+    html_document = headFile.read()
+    headFile.close()
+except Exception as e:
+    print e
 
-
-htmlDocument = '''
-<!DOCTYPE html>
-<html lang=en>
-<head>
-    <title>splatter</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap-3.3.7.min.css">
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap-3.3.7.min.js"></script>
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.7.min.js"></script> -->
-    <style>
-        .w3-black,.w3-hover-black:hover{color:#fff!important;background-color:#000!important}
-        .w3-code,.w3-codespan{font-family:Consolas,"courier new";font-size:16px}
-        .w3-code{line-height:1.4;width:auto;background-color:#fff;padding:8px 12px;border-left:4px solid #4CAF50;word-wrap:break-word}
-    </style>
-</head>
-<body class="w3-black">
-'''
 
 # Loop through each Subject
-htmlDocument += '<div class="container-fluid"><br>\n'
 for subject in root:
-    subjectText = subject.text.strip(' \n\t')   # Iterate through each of the Subjects
-    print subjectText
-    htmlDocument += '\t<button type="button" class="btn btn-success btn-block" data-toggle="collapse" style="padding: 10px 10px;" data-target="#' + \
-                    subjectText + '">' + subjectText + '</button><br>\n'
-    htmlDocument += '\t<div id="' + subjectText + '" class="collapse">\n'
-    # Count the number of Sections in the Subject - The last section needs </ul> tag at the end
-    numSections = countNumberSections(subject)
-    sectionNumber = 1
+    subject_name = subject.text.encode('utf-8').strip(' \n\t')
+    print '(+) Starting XML Parse on subject: ' + subject_name
+    # INSERT COLLAPSIBLE SUBJECT BUTTON WITH SUBJECT NAME
+    html_document += '\t<button type="button" class="btn btn-success btn-block" data-toggle="collapse" ' + \
+        'style="padding: 10px 10px;" data-target="#' + subject_name + '">' + subject_name + '</button><br>\n' + \
+        '\t<div id="' + subject_name + '" class="collapse">\n'
+    # GET THE NUMBER OF SECTIONS WITHIN SUBJECT
+    total_sections = len(subject.findall('section'))
+    print '\t(+) FOUND ' + str(total_sections) + ' SECTIONS WITHIN THE ' + subject_name + ' SUBJECT'
+
+    # START ITERATE/PARSING SECTIONS
+    section_counter = 1
     for section in subject:
-        sectionText = section.text.encode('utf-8').strip(' \n\t')
-        htmlDocument += '\t\t<button type="button" class="btn btn-danger btn-block" data-toggle="collapse" data-target="#' + \
-                    sectionText + '">' + sectionText + '</button><br>\n'
-        htmlDocument += '\t\t<div id="' + sectionText + '" class="collapse">\n'
+        section_name = section.text.encode('utf-8').strip(' \n\t')
+        # INSERT COLLAPSIBLE SECTION BUTTON WITH SECTION NAME
+        html_document += '\t\t<button type="button" class="btn btn-danger btn-block" data-toggle="collapse" ' + \
+            'data-target="#' + section_name + '">' + section_name + '</button><br>\n' + '\t\t<div id="' + \
+            section_name + '" class="collapse">\n'
+
+        # START ITERATE/PARSING CONTENT WITHIN SECTION
         for content in section:
             if content.tag == "content-title":
-                contentTitle = content.text.encode('utf-8').strip('\n\t')
-                htmlDocument += '\t\t\t<font color="red"><b>' + contentTitle + '</b></font>\n'
+                content_title = content.text.encode('utf-8').strip('\n\t')
+                # ADD FONT COLOR TO CONTENT TITLE
+                html_document += '\t\t\t<font color="red"><b>' + content_title + '</b></font>\n'
             if content.tag == "content":
-                contentText = content.text.split('\n')
-                htmlDocument += '\t\t\t<div class="w3-code w3-black">\n'
-                for ctext in contentText:
-                    ctext = ctext.encode('utf-8').strip(' \t')
-                    if ctext == ' \n':
-                        pass
-                    if ctext[:1] == "#":
-                        htmlDocument += '\t\t\t\t<font color ="blue">' + ctext + '</font><br>\n'
-                    else:
-                        htmlDocument += '\t\t\t\t<font color="green">' + ctext + '</font><br>\n'
-                htmlDocument += '\t\t\t</div><br>\n'
-        htmlDocument += '\t\t</div>\n'
-        # Once the last section has been reached, add the final closing tags
-        if sectionNumber == numSections:
-            htmlDocument += '\t</div><br>\n'
-        else:
-            sectionNumber += 1
-htmlDocument += '</div>'
+                content_lines = content.text.split('\n')
+                html_document += '\t\t\t<div class="w3-code w3-black">\n'
 
-htmlDocument += '''
+                # ITERATE THROUGH EACH LINE IN CONTENT
+                for content_line in content_lines:
+                    content_line = content_line.encode('utf-8').strip(' \t')
+                    # CLEAR ANY BLANK LINES
+                    if len(content_line.strip()) == 0:
+                        content_line = ''
+                        print '(*) - FOUND BLANK CONTENT LINE - CHECK XML AT ' + content_title
+                    # IF FIRST CHAR IN LINE IS A # - CHANGE FONT COLOR FOR LINE
+                    if content_line[:1] == "#":
+                        html_document += '\t\t\t\t<font color ="blue">' + content_line + '</font><br>\n'
+                    # CHANGE FONT COLOR OF LINE
+                    else:
+                        html_document += '\t\t\t\t<font color="green">' + content_line + '</font><br>\n'
+                # CLOSE OUT CONTENT DIV
+                html_document += '\t\t\t</div><br>\n'
+        # CLOSE OUT SECTION DIV
+        html_document += '\t\t</div>\n'
+        # ONCE LAST SECTION HAS BEEN REACHED - CLOSE OUT SUBJECT DIV
+        if section_counter == total_sections:
+            html_document += '\t</div><br>\n'
+        else:
+            section_counter += 1
+
+# END OF LOOP - CLOSE OUT FINAL DIV / BODY / HTML TAGS
+html_document += '''
+</div>
 </body>
 </html>
 '''
 
 try:
     htmlFile = open("splatter.html", "w")
-    htmlFile.write(htmlDocument)
+    htmlFile.write(html_document)
     htmlFile.close()
+    print '(+) HTML FILE GENERATED'
 except Exception as e:
-    print e
+    print '(-) FILE ERROR: ' + e
 
